8ee8bc69ee7bbce7204396986e5d54f6480d0ea9
cappochinho <edwardprempeh18@gmail.com>
2023-04-03 23:15:37 +0100

    Task 0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+This module contains task 0 - Basic async syntax
+"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> int:
+    """
+    Takes an int input, waits for a random delay
+    and returns it
+    """
+
+    rnd: float = random.uniform(0, max_delay)
+    await asyncio.sleep(rnd)
+    return rnd
