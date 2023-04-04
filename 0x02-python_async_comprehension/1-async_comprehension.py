893d44507d0d3655758d384221e1659947c04000
cappochinho <edwardprempeh18@gmail.com>
2023-04-04 22:24:09 +0100

    re-adding task 2

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -11,4 +11,4 @@ async_generator = __import__('0-async_generator').async_generator
 async def comprehension() -> List[float]:
     """Comprehension over async_generator"""
 
-    return [_ async for _ in async_generator()]
+    return [i async for i in async_generator()]
